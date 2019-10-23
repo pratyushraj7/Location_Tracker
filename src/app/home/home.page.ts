8e07248c0a7e17ef4b41be2344a807b30c0f2611
Pratyush Raj <42607563+pratyushraj7@users.noreply.github.com>
2019-10-23 21:21:18 +0530

    second

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+ 
+declare var google;
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  locations: Observable<any>;
+  locationsCollection: AngularFirestoreCollection<any>;
+  
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers = [];
+ 
+  isTracking = false;
+  watch: string;
+  user = null;
+ 
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.anonLogin();
+  }
 
-  constructor() {}
+  startTracking(){
+    this.isTracking = true;
+    this.watch = Geolocation.watchPosition({}, (position, err) => {
+      if (position) {
+        this.addNewLocation(
+          position.coords.latitude,
+          position.coords.longitude,
+          position.timestamp
+        );
+      }
+    });
+  }
+   
+  stopTracking(){
+    Geolocation.clearWatch({ id: this.watch }).then(() => {
+      this.isTracking = false;
+    });
+  }
+   
+  addNewLocation(lat, lng, timestamp) {
+    this.locationsCollection.add({
+      lat,
+      lng,
+      timestamp
+    });
+   
+    let position = new google.maps.LatLng(lat, lng);
+    this.map.setCenter(position);
+    this.map.setZoom(5);
+  }
+   
+  deleteLocation(pos) {
+    this.locationsCollection.doc(pos.id).delete();
+  }
 
+  anonLogin(){
+    this.afAuth.auth.signInAnonymously().then(res => {
+      this.user = res.user;
+ 
+      this.locationsCollection = this.afs.collection(
+        `locations/${this.user.uid}/track`,
+        ref => ref.orderBy('timestamp')
+      );
+ 
+      this.locations = this.locationsCollection.snapshotChanges().pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+        )
+      );
+ 
+      this.locations.subscribe(locations => {
+        this.updateMap(locations);
+      });
+    });
+  }
+
+  updateMap(locations) {
+    this.markers.map(marker => marker.setMap(null));
+    this.markers = [];
+   
+    for (let loc of locations) {
+      let latLng = new google.maps.LatLng(loc.lat, loc.lng);
+   
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+      this.markers.push(marker);
+    }
+  }
+ 
+  ionViewWillEnter() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(51.9036442, 7.6673267);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 }
+ 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBmuTdhp5sBazPHkWC7HFwL_lQrnItBxHI",
+    authDomain: "my-location-256718.firebaseapp.com",
+    databaseURL: "https://my-location-256718.firebaseio.com",
+    projectId: "my-location-256718",
+    storageBucket: "my-location-256718.appspot.com",
+    messagingSenderId: "764431420581"
+  }
 };
 
 /*
